291c0c332bba3fcda795cffa7293d64c4aa904ee
Mario Lazzari <mario.lazzari@gmail.com>
2020-09-05 23:01:10 +0200

    Done lesson 3

--- sandBox.ts
+++ sandBox.ts
@@ -18,3 +18,30 @@ await Deno.rename("readme.txt", "deleteme.txt");
 const res = await fetch("https://swapi.dev/api/films");
 const data = await res.json();
 console.log(data);
+
+// uuid
+import { v4 } from "https://deno.land/std/uuid/mod.ts";
+
+// Generate a v4 uuid
+const uid = v4.generate();
+console.log("uid:", uid);
+
+// fs modeula
+import { readJson, writeJson } from "https://deno.land/std/fs/mod.ts";
+
+await writeJson(
+  "ninjas.json",
+  { "ninjas": ["Ninja1", "Ninja2", "Ninja3"] },
+  { spaces: 2 },
+);
+const jsnObj = await readJson("ninjas.json");
+console.log(jsnObj);
+
+// httpmodule
+import { serve } from "https://deno.land/std/http/server.ts";
+const server = serve({ port: 3000 });
+console.log("http://localhost:3000/");
+for await (const req of server) {
+  const { url } = req;
+  req.respond({ body: `You visited ${url}.` });
+}
